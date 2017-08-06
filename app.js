2d8cd0f297d23fb55cd4ae47d260739e1f416d54
Jeffrey Chan <jeffschan97@gmail.com>
2017-08-05 23:17:55 -0700

    favicon.ico

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const favicon = require('serve-favicon')
 const threadly = require('./threadly')
 
 app = express()
 
 app.set('port', (process.env.PORT || 3000))
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use('/', express.static(path.join(__dirname, 'views/')))
 app.use('/', express.static(path.join(__dirname, 'public/')))
 app.use(bodyParser.json())
